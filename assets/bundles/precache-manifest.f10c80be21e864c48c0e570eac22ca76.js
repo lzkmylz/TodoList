a79c99914d91bd79f9e95d0c99608beb6de6e8a8
lzkmylz <lzkmylz@163.com>
2018-11-14 16:15:08 +0800

    init

---
+++ assets/bundles/precache-manifest.f10c80be21e864c48c0e570eac22ca76.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2167bf428586da42f6f2",
+    "url": "/static/bundles/static/css/main.2618839c.css"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/bundles/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2167bf428586da42f6f2",
+    "url": "/static/bundles/js/main.2167bf42.js"
+  },
+  {
+    "revision": "6b38fad103d7b772b5104cee0599cb04",
+    "url": "/static/bundles/index.html"
+  }
+];
